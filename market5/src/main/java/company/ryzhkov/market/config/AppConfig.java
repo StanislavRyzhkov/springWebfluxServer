471e011438240214365c8b338020ee13012bea33
StanislavRyzhkov <30403280+StanislavRyzhkov@users.noreply.github.com>
2019-10-17 23:30:03 +0700

    17 10 19

--- market5/src/main/java/company/ryzhkov/market/config/AppConfig.java
+++ market5/src/main/java/company/ryzhkov/market/config/AppConfig.java
@@ -1,9 +1,12 @@
 package company.ryzhkov.market.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebFlux
@@ -12,7 +15,10 @@ public class AppConfig implements WebFluxConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         if (!registry.hasMappingForPattern("/api/media/**")) {
-            registry.addResourceHandler("/api/media/**").addResourceLocations("file:media/");
+            registry
+                    .addResourceHandler("/api/media/**")
+                    .addResourceLocations("file:media/")
+                    .setCacheControl(CacheControl.maxAge(365L, TimeUnit.DAYS));
         }
     }
 }
